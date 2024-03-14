edf3b8e5c0c958f16e6b23f9914c2f0f2fc7e334
Francis <francismainakabutha@gmail.com>
2024-03-14 07:27:22 +0300

    adds the counting logic

--- src/wctool.js
+++ src/wctool.js
@@ -1,1 +1,28 @@
-console.log("wctool.js")
+module.exports = function (content, options = {}) {
+  const results = [];
+
+  if (options.bytes) {
+    results.push(content.length);
+  }
+
+  if (options.lines) {
+    results.push(content.split(/\r?\n/).length); //regular expression for line breaks
+  }
+
+  if (options.words) {
+    results.push(content.split(/\b\w+\b/u).length); //Use Unicode word boundary
+  }
+
+  if (options.chars) {
+    results.push(content.replace(/\r?\n/g, "").length);
+  }
+
+  // Default output (all counts) if no options are specified
+  if (results.length === 0) {
+    results.push(content.length);
+    results.push(content.split(/\r?\n/).length);
+    results.push(content.split(/\s+/).length);
+  }
+
+  return results;
+};
